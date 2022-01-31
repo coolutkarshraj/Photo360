cfa3d05556fa68fea922cee551bb963b43fa2f91
coolutkarshraj <utkarshcpr@gmail.com>
2022-01-31 12:54:03 +0530

    Photo360 image stiching Basic App demo

---
+++ app/src/main/java/com/io/utkarsh/FileUtil.kt
@@ -0,0 +1,79 @@
+package com.io.utkarsh
+
+import android.content.Context
+import android.net.Uri
+import android.os.Environment.*
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+
+class FileUtil(private val context: Context) {
+
+    @Throws(IOException::class)
+    fun urisToFiles(uris: List<Uri>): List<File> {
+        val files = ArrayList<File>(uris.size)
+        for (uri in uris) {
+            val file = createTempFile(requireTemporaryDirectory())
+            writeUriToFile(uri, file)
+            files.add(file)
+        }
+        return files
+    }
+
+    fun createResultFile(): File {
+        val pictures = context.getExternalFilesDir(DIRECTORY_PICTURES)!!
+        return createTempFile(File(pictures, RESULT_DIRECTORY_NAME))
+    }
+
+    fun cleanUpWorkingDirectory() {
+        requireTemporaryDirectory().remove()
+    }
+
+    @Throws(IOException::class)
+    private fun createTempFile(root: File): File {
+        root.mkdirs() // make sure that the directory exists
+        val date = SimpleDateFormat(DATE_FORMAT_TEMPLATE, Locale.getDefault()).format(Date())
+        val filePrefix = IMAGE_NAME_TEMPLATE.format(date)
+        return File.createTempFile(filePrefix, JPG_EXTENSION, root)
+    }
+
+    @Throws(IOException::class)
+    private fun writeUriToFile(target: Uri, destination: File) {
+        val inputStream = context.contentResolver.openInputStream(target)!!
+        val outputStream = FileOutputStream(destination)
+        inputStream.use { input ->
+            outputStream.use { out ->
+                input.copyTo(out)
+            }
+        }
+    }
+
+    private fun requireTemporaryDirectory(): File {
+        // don't need read/write permission for this directory starting from android 19
+        val pictures = context.getExternalFilesDir(DIRECTORY_PICTURES)!!
+        return File(pictures, TEMPORARY_DIRECTORY_NAME)
+    }
+
+    // there is no build in function for deleting folders <3
+    private fun File.remove() {
+        if (isDirectory) {
+            val entries = listFiles()
+            if (entries != null) {
+                for (entry in entries) {
+                    entry.remove()
+                }
+            }
+        }
+        delete()
+    }
+
+    companion object {
+        private const val TEMPORARY_DIRECTORY_NAME = "Temporary"
+        private const val RESULT_DIRECTORY_NAME = "Results"
+        private const val DATE_FORMAT_TEMPLATE = "yyyyMMdd_HHmmss"
+        private const val IMAGE_NAME_TEMPLATE = "IMG_%s_"
+        private const val JPG_EXTENSION = ".jpg"
+    }
+}
---
+++ app/src/main/java/com/io/utkarsh/MainActivity.kt
@@ -0,0 +1,140 @@
+package com.io.utkarsh
+
+import android.Manifest
+import android.app.Activity
+import android.app.ProgressDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import android.widget.RadioGroup
+import android.widget.Toast
+
+import com.squareup.picasso.MemoryPolicy
+import com.squareup.picasso.Picasso
+
+import org.bytedeco.javacpp.opencv_stitching.Stitcher
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.io.utkarsh.StitcherOutput.Failure
+import com.io.utkarsh.StitcherOutput.Success
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.PublishSubject
+import java.io.File
+import id.zelory.compressor.Compressor
+import kotlinx.coroutines.Dispatchers
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var imageView: ImageView
+    private lateinit var imageStitcher: ImageStitcher
+    private lateinit var disposable: Disposable
+
+    private val stitcherInputRelay = PublishSubject.create<StitcherInput>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val permission = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE)
+        if ((ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED)&&(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(this,  permission, 1);
+        }
+        setUpViews()
+        setUpStitcher()
+    }
+
+    private fun setUpViews() {
+        imageView = findViewById(R.id.image)
+        findViewById<View>(R.id.button).setOnClickListener { chooseImages() }
+    }
+
+    @Suppress("DEPRECATION")
+    private  fun setUpStitcher() {
+        imageStitcher = ImageStitcher(FileUtil(applicationContext))
+        val dialog = ProgressDialog(this).apply {
+            setMessage(getString(R.string.processing_images))
+            setCancelable(false)
+        }
+
+        disposable = stitcherInputRelay.switchMapSingle {
+            imageStitcher.stitchImages(it)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .doOnSubscribe { dialog.show() }
+                    .doOnSuccess { dialog.dismiss() }
+        }
+                .subscribe({ processResult(it) }, { processError(it) })
+    }
+
+    private fun chooseImages() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+                .setType(INTENT_IMAGE_TYPE)
+                .putExtra(EXTRA_ALLOW_MULTIPLE, true)
+        startActivityForResult(intent, CHOOSE_IMAGES)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CHOOSE_IMAGES && resultCode == Activity.RESULT_OK && data != null) {
+            val clipData = data.clipData
+            val images = if (clipData != null) {
+                List(clipData.itemCount) { clipData.getItemAt(it).uri }
+            } else {
+                listOf(data.data!!)
+            }
+
+
+               processImages(images)
+
+        }
+    }
+
+     private fun processImages(uris: List<Uri>) {
+        imageView.setImageDrawable(null) // reset preview
+        val stitchMode = Stitcher.PANORAMA
+        stitcherInputRelay.onNext(StitcherInput(uris, stitchMode))
+    }
+
+
+    private fun processError(e: Throwable) {
+        Log.e(TAG, "", e)
+        Toast.makeText(this, e.message + "", Toast.LENGTH_LONG).show()
+    }
+
+    private fun processResult(output: StitcherOutput) {
+        when (output) {
+            is Success -> showImage(output.file)
+            is Failure -> processError(output.e)
+        }
+    }
+
+    private fun showImage(file: File) {
+        Picasso.with(this).load(file)
+                .memoryPolicy(MemoryPolicy.NO_STORE, MemoryPolicy.NO_CACHE)
+                .into(imageView)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposable.dispose()
+    }
+
+    companion object {
+        private const val TAG = "TAG"
+        private const val EXTRA_ALLOW_MULTIPLE = "android.intent.extra.ALLOW_MULTIPLE"
+        private const val INTENT_IMAGE_TYPE = "image/*"
+        private const val CHOOSE_IMAGES = 777
+    }
+}
